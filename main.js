3fb1eb084b4a4ee2f8793ca13b33d5df874baff7
terrell linnell <terrelllinnell@terrells-MacBook-Pro.local>
2016-12-10 12:53:36 -0700

    basic exaplmes of jQuery

---
+++ main.js
@@ -0,0 +1,15 @@
+
+// var greeting = function() {
+//
+//   $('.jumbo_text').text('I changed the text!').css('background-color', 'orange');
+// };
+//
+// $(".alert_button").click(greeting);
+
+var getName = function(){
+  var nameInput = $('.name_input').val();
+  console.log(nameInput)
+  alert("Hello there " + nameInput);
+};
+
+$('.alert_button2').click(getName);
